e72b1772ca4ffd6885b330f7041e665c8551699f
JeremyCurmi <54748653+JeremyCurmi@users.noreply.github.com>
2024-07-04 16:14:36 +0200

    new improved method

--- python.go
+++ python.go
@@ -1,6 +1,7 @@
 package python
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"io"
@@ -8,6 +9,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 )
 
 type Python struct {
@@ -78,6 +80,26 @@ func (p *Python) Execute(path string, args ...string) error {
 	return nil
 }
 
+func (p *Python) ExecuteImproved(path string, args ...string) error {
+
+	commandArgs := []string{path}
+	commandArgs = append(commandArgs, args...)
+
+	cmd := exec.Command(p.Executable, commandArgs...)
+	cmd.Env = os.Environ()
+
+	if len(p.Env) > 0 {
+		cmd.Env = append(cmd.Env, p.Env...)
+	}
+
+	err := receiveLogsAndErrorsFromPipe(cmd)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func copyFromTo(r io.Reader, w io.Writer) {
 	var copy []byte
 	buf := make([]byte, 1024, 1024)
@@ -108,4 +130,52 @@ func writeOrDie(w io.Writer, r io.Reader, buf []byte, out []byte) ([]byte, error
 		}
 	}
 	return out, err
+}
+
+func receiveLogsAndErrorsFromPipe(cmd *exec.Cmd) error {
+	rOut, _ := cmd.StdoutPipe()
+	rErr, _ := cmd.StderrPipe()
+	scannerOut := bufio.NewScanner(rOut)
+	scannerErr := bufio.NewScanner(rErr)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	errChan := make(chan string, 1)
+	errLine := ""
+
+	go func() {
+		for scannerOut.Scan() {
+			line := scannerOut.Text()
+			fmt.Println(line)
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		for scannerErr.Scan() {
+			lineErr := scannerErr.Text()
+			errLine = fmt.Sprintf("%s \n %s", errLine, lineErr)
+		}
+		errChan <- errLine
+		wg.Done()
+	}()
+
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	wg.Wait()
+
+	errPython := <-errChan
+	if errPython != "" {
+		return errors.New(errPython)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
